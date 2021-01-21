350daedce67e78d9352941554cf059dc62c4ef78
stephp23 <stephp23@gmail.com>
2021-01-20 21:25:00 -0500

    first commit

---
+++ src/components/FruitList.js
@@ -0,0 +1,10 @@
+const FruitList = props => {
+  console.log(props, 'fruitlist')
+  return (
+    < ul >
+      {props.fruits.map((fruit) => (
+        <li>{fruit}</li>))}
+    </ul>
+  )
+}
+export default FruitList
